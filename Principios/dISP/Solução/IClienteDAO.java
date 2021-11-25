f1a8d80da8a980d93cb5485934b692da4051ca4e
Ander <andersonhg71@gmail.com>
2021-11-25 04:05:36 -0300

    Update IClienteDAO.java

--- Principios/dISP/Solução/IClienteDAO.java
+++ Principios/dISP/Solução/IClienteDAO.java
@@ -1,3 +1,8 @@
+package br.ufscar.dc.dsw.dao;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
 import br.ufscar.dc.dsw.domain.Cliente;
 
 public interface IClienteDAO extends CrudRepository<Cliente, Long> {
